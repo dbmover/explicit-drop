18d78d8c167e95b3485d1554643ce5afbdb072fa
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-10 16:44:37 +0200

    add, not defer

--- src/Plugin.php
+++ src/Plugin.php
@@ -6,11 +6,13 @@ use Dbmover\Core;
 
 class Plugin extends Core\Plugin
 {
+    public $description = 'Explicitly dropping objects...';
+
     public function __invoke(string $sql) : string
     {
         preg_match_all('@^DROP .*?;$@ms', $sql, $matches, PREG_SET_ORDER);
         foreach ($matches as $stmt) {
-            $this->defer($stmt[0]);
+            $this->addOperation($stmt[0]);
             $sql = str_replace($stmt[0], '', $sql);
         }
         return $sql;
