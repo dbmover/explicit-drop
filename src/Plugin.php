76d0a4e1f82eabfc8a608aa9897552c0d968560e
Marijn Ophorst <marijn@monomelodies.nl>
2017-11-22 17:22:52 +0100

    also do this for alter table/drop statements

--- src/Plugin.php
+++ src/Plugin.php
@@ -15,6 +15,11 @@ class Plugin extends Core\Plugin
             $this->addOperation($stmt[0]);
             $sql = str_replace($stmt[0], '', $sql);
         }
+        preg_match_all('@^ALTER TABLE \w+ DROP CONSTRAINT .*?;$@ms', $sql, $matches, PREG_SET_ORDER);
+        foreach ($matches as $stmt) {
+            $this->addOperation($stmt[0]);
+            $sql = str_replace($stmt[0], '', $sql);
+        }
         return $sql;
     }
 }
